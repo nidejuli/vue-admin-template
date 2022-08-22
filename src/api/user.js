841051cf759702a0fbf4b7dd407aaca4d24f3a4a
wangweijie <xijing9705@163.com>
2022-08-22 15:28:24 +0800

    修改代码

--- src/api/user.js
+++ src/api/user.js
@@ -1,20 +1,37 @@
 import request from '@/utils/request'
 
-export function login(data) {
+// export function login(data) {
+//   return request({
+//     url: '/vue-element-admin/user/login',
+//     method: 'post',
+//     data
+//   })
+// }
+export function login(username, password) {
   return request({
-    url: '/vue-element-admin/user/login',
+    url: 'eduService/user/login',
     method: 'post',
-    data
+    data: {
+      username,
+      password
+    }
   })
 }
 
 export function getInfo(token) {
   return request({
-    url: '/vue-element-admin/user/info',
+    url: 'eduService/user/info',
     method: 'get',
     params: { token }
   })
 }
+// export function getInfo(token) {
+//   return request({
+//     url: '/vue-element-admin/user/info',
+//     method: 'get',
+//     params: { token }
+//   })
+// }
 
 export function logout() {
   return request({
